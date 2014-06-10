d4d60f6014f5d73eff35816947d74d95bf0cfca4
Nachev <n.n.nachev@gmail.com>
2014-06-10 19:23:27 +0300

    Added projectile.js

--- Teamwork - The Game/scripts/main.js
+++ Teamwork - The Game/scripts/main.js
@@ -1,7 +1,9 @@
 /*jslint browser: true*/
 window.onload = function main() {
     var canvas = null,
-        context = null;
+        context = null,
+        projectile = null,
+        time = null;
 
     function initialize() {
         canvas = document.getElementById('the-canvas');
@@ -10,25 +12,31 @@ window.onload = function main() {
         // Initializes needed content in the beginning
         // Invoked once
 
-        animation();
+        projectile = new Projectile(new Point(10, 300), 30 * Math.PI / 180, 50);
+        time = 0.1;
+
+        setInterval(animation, 15);
     }
 
     function draw() {
         // Calls drawing functions of the objects
         // Invoked every frame
+
+        projectile.draw(context);
     }
 
     function update() {
         // Updates objects state
         // Invoked every frame
+        time += 0.5;
+
+        projectile.update(time);
     }
 
     function animation() {
         context.clearRect(0, 0, canvas.width, canvas.height);
-
-        //...
-
-        requestAnimationFrame(animation);
+        update();
+        draw();
     }
 
     initialize();
