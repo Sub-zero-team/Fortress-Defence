d68e3c5fa6540f4d99277125addca4768409ab8b
Nachev <n.n.nachev@gmail.com>
2014-06-10 14:54:32 +0300

    Sample structure of the project

--- Teamwork - The Game/scripts/main.js
+++ Teamwork - The Game/scripts/main.js
@@ -1,12 +1,27 @@
-window.onload = function () {
-    game();
-}
+/*jslint browser: true*/
+window.onload = function main() {
+    var canvas = null,
+        context = null;
 
-function game() {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
+    function initialize() {
+        canvas = document.getElementById('the-canvas');
+        context = canvas.getContext('2d');
 
-    animation();
+        // Initializes needed content in the beginning
+        // Invoked once
+
+        animation();
+    }
+
+    function draw() {
+        // Calls drawing functions of the objects
+        // Invoked every frame
+    }
+
+    function update() {
+        // Updates objects state
+        // Invoked every frame
+    }
 
     function animation() {
         context.clearRect(0, 0, canvas.width, canvas.height);
@@ -15,4 +30,6 @@ function game() {
 
         requestAnimationFrame(animation);
     }
-}
+
+    initialize();
+};
---
+++ Teamwork - The Game/scripts/physics.js
@@ -0,0 +1,10 @@
+/*jslint browser: true*/
+window.onload = function physics() {
+    this.initialize = function initPhysics() {
+        // body...
+    };
+
+    this.update = function updatePhysics() {
+        // body...
+    };
+};
