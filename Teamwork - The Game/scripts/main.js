eeaa84247008e8038f7938076a15f100a204bb78
Sh0nZi <sss17@mail.com>
2014-06-11 12:25:27 +0300

    Added ship generator.

--- Teamwork - The Game/scripts/main.js
+++ Teamwork - The Game/scripts/main.js
@@ -3,17 +3,37 @@ window.onload = function main() {
     var canvas = null,
         context = null,
         projectile = null,
-        time = null;
+        time = null,
+        rand,
+        stage = new Kinetic.Stage({
+            container:'canvas-container',
+            width:1200,
+            height:700
+        }),
+        shipsLayer = new Kinetic.Layer(),
+        ships=[];
+
+
+    function generateShips(){
+        var y,
+
+        rand = Math.random(),
+            y = rand*(530-400)+400;
+        ship = new Ship(720,y,'images/ships.png',stage,shipsLayer,0.5,30);
+        ships.push(ship);
+        ship.draw();
+    }
 
     function initialize() {
-        canvas = document.getElementById('the-canvas');
-        context = canvas.getContext('2d');
+       // canvas = document.getElementById('the-canvas');
+       // context = canvas.getContext('2d');
 
         // Initializes needed content in the beginning
         // Invoked once
 
-        projectile = new Projectile(new Point(10, 300), 30 * Math.PI / 180, 50);
-        time = 0.1;
+        //projectile = new Projectile(new Point(10, 300), 30 * Math.PI / 180, 50);
+
+        time = 0.5;
 
         setInterval(animation, 15);
     }
@@ -22,7 +42,7 @@ window.onload = function main() {
         // Calls drawing functions of the objects
         // Invoked every frame
 
-        projectile.draw(context);
+        //projectile.draw(context);
     }
 
     function update() {
@@ -30,11 +50,15 @@ window.onload = function main() {
         // Invoked every frame
         time += 0.5;
 
-        projectile.update(time);
+        //projectile.update(time);
     }
 
     function animation() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        //context.clearRect(0, 0, canvas.width, canvas.height);
+
+        if(time%100===0){
+            generateShips();
+        }
         update();
         draw();
     }
---
+++ Teamwork - The Game/scripts/ship.js
@@ -0,0 +1,75 @@
+/**
+ * Created by ShOnZi on 6/11/2014.
+ */
+function Ship(x,y,sprite,stage,layer,speed,damage) {
+
+    'use strict';
+    var image = new Image(),
+        ship,
+        self=this;
+    this.damage=damage;
+    this.x=x;
+    this.y=y;
+    this.speed=speed;
+    this.draw = function () {
+        image.onload = function () {
+            ship = new Kinetic.Sprite({
+                x: x,
+                y: y,
+                image: image,
+                animation: 'move',
+                animations: {
+                    move: [
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 0, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100,
+                        0, 2, 100, 100
+                    ]
+                },
+                frameRate: 20,
+                frameIndex: 0
+
+            });
+
+            layer.add(ship);
+            stage.add(layer);
+            ship.start();
+            ship.on('frameIndexChange', function () {
+
+                ship.setX(ship.attrs.x -= speed);
+                self.x-=speed;
+            });
+        };
+        image.src=sprite;
+    };
+}
