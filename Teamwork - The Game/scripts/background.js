6abdd17496ce0928088538dee0131e982553052e
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-06-12 11:05:43 +0300

    Background
    BackgroundAdded
    background.js,
    background.png

---
+++ Teamwork - The Game/scripts/background.js
@@ -0,0 +1,24 @@
+﻿var stage = new Kinetic.Stage({
+    container: 'background-container',
+    width: 802,
+    height: 602
+});
+var imgLayer = new Kinetic.Layer();
+var imageObj = new Image();
+imageObj.src='images/background.png'
+imageObj.onload = function () {
+    var background = new Kinetic.Image({
+        x: 0,
+        y: 0,
+        image: imageObj,
+        width: 802,
+        height: 602
+    });
+
+    // add the shape to the layer
+    imgLayer.add(background);
+
+    // add the layer to the stage
+    stage.add(imgLayer);
+};
+
